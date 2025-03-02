f0eff6566c1362f9d80850280ccfe91d61a4dee3
Zobaer <zobaerzahad.git@gmail.com>
2025-03-02 13:38:16 +0600

    some edit in script file

--- script/script.js
+++ script/script.js
@@ -19,229 +19,37 @@ document.getElementById("discover-btn").addEventListener("click", function () {
 document
   .getElementById("shopEase-btn")
   .addEventListener("click", function (event) {
-    event.preventDefault();
-    const task = parseInt(getInnerTextById("taskAssigned"));
-    const newtask = task - 1;
-    const allTask = parseInt(getInnerTextById("all-task"));
-    const newAllTask = allTask + 1;
-    alert("Board Updated Successfully");
-    document.getElementById("taskAssigned").innerText = newtask;
-    document.getElementById("all-task").innerText = newAllTask;
-    document.getElementById("shopEase-btn").disabled = true;
-    document.getElementById("shopEase-btn").style.backgroundColor = "#9ca3af";
-    const timeFormate = {
-      hour: "2-digit",
-      minute: "2-digit",
-      second: "2-digit",
-      hour12: true,
-    };
-    const time = new Intl.DateTimeFormat("en-US", timeFormate).format(
-      new Date()
-    );
-    const h1 = document.getElementById("fixMobile").innerText;
-    const p = document.createElement("p");
-    p.innerText = `You have complete the task ${h1} at ${time}`;
-    document.getElementById("history").appendChild(p);
-    p.classList.add(
-      "bg-gray-100",
-      "mt-4",
-      "text-xs",
-      "px-2",
-      "mx-3",
-      "rounded-lg",
-      "py-2",
-      "text-[#000000]"
-    );
+    task(event, "shopEase-btn", "fixMobile");
   });
 // cloudSync-btn (2nd task)
 document
   .getElementById("cloudSync-btn")
   .addEventListener("click", function (event) {
-    event.preventDefault();
-    const task = parseInt(getInnerTextById("taskAssigned"));
-    const newtask = task - 1;
-    const allTask = parseInt(getInnerTextById("all-task"));
-    const newAllTask = allTask + 1;
-    alert("Board Updated Successfully");
-    document.getElementById("taskAssigned").innerText = newtask;
-    document.getElementById("all-task").innerText = newAllTask;
-    document.getElementById("cloudSync-btn").disabled = true;
-    document.getElementById("cloudSync-btn").style.backgroundColor = "#9ca3af";
-    const timeFormate = {
-      hour: "2-digit",
-      minute: "2-digit",
-      second: "2-digit",
-      hour12: true,
-    };
-    const time = new Intl.DateTimeFormat("en-US", timeFormate).format(
-      new Date()
-    );
-    const h1 = document.getElementById("darkMode").innerText;
-    const p = document.createElement("p");
-    p.innerText = `You have complete the task ${h1} at ${time}`;
-    document.getElementById("history").appendChild(p);
-    p.classList.add(
-      "bg-gray-100",
-      "mt-4",
-      "text-xs",
-      "px-2",
-      "mx-3",
-      "rounded-lg",
-      "py-2",
-      "text-[#000000]"
-    );
+    task(event, "cloudSync-btn", "darkMode");
   });
 // SwiftPay-btn (3rd task)
 document
   .getElementById("SwiftPay-btn")
   .addEventListener("click", function (event) {
-    event.preventDefault();
-    const task = parseInt(getInnerTextById("taskAssigned"));
-    const newtask = task - 1;
-    const allTask = parseInt(getInnerTextById("all-task"));
-    const newAllTask = allTask + 1;
-    alert("Board Updated Successfully");
-    document.getElementById("taskAssigned").innerText = newtask;
-    document.getElementById("all-task").innerText = newAllTask;
-    document.getElementById("SwiftPay-btn").disabled = true;
-    document.getElementById("SwiftPay-btn").style.backgroundColor = "#9ca3af";
-    const timeFormate = {
-      hour: "2-digit",
-      minute: "2-digit",
-      second: "2-digit",
-      hour12: true,
-    };
-    const time = new Intl.DateTimeFormat("en-US", timeFormate).format(
-      new Date()
-    );
-    const h1 = document.getElementById("third-head").innerText;
-    const p = document.createElement("p");
-    p.innerText = `You have complete the task ${h1} at ${time}`;
-    document.getElementById("history").appendChild(p);
-    p.classList.add(
-      "bg-gray-100",
-      "mt-4",
-      "text-xs",
-      "px-2",
-      "mx-3",
-      "rounded-lg",
-      "py-2",
-      "text-[#000000]"
-    );
+    task(event, "SwiftPay-btn", "third-head");
   });
 // fourth-btn (fourth task)
 document
   .getElementById("fourth-btn")
   .addEventListener("click", function (event) {
-    event.preventDefault();
-    const task = parseInt(getInnerTextById("taskAssigned"));
-    const newtask = task - 1;
-    const allTask = parseInt(getInnerTextById("all-task"));
-    const newAllTask = allTask + 1;
-    alert("Board Updated Successfully");
-    document.getElementById("taskAssigned").innerText = newtask;
-    document.getElementById("all-task").innerText = newAllTask;
-    document.getElementById("fourth-btn").disabled = true;
-    document.getElementById("fourth-btn").style.backgroundColor = "#9ca3af";
-    const timeFormate = {
-      hour: "2-digit",
-      minute: "2-digit",
-      second: "2-digit",
-      hour12: true,
-    };
-    const time = new Intl.DateTimeFormat("en-US", timeFormate).format(
-      new Date()
-    );
-    const h1 = document.getElementById("fourth-head").innerText;
-    const p = document.createElement("p");
-    p.innerText = `You have complete the task ${h1} at ${time}`;
-    document.getElementById("history").appendChild(p);
-    p.classList.add(
-      "bg-gray-100",
-      "mt-4",
-      "text-xs",
-      "px-2",
-      "mx-3",
-      "rounded-lg",
-      "py-2",
-      "text-[#000000]"
-    );
+    task(event, "fourth-btn", "fourth-head");
   });
 // fifth-btn (fifth task)
 document
   .getElementById("fifth-btn")
   .addEventListener("click", function (event) {
-    event.preventDefault();
-    const task = parseInt(getInnerTextById("taskAssigned"));
-    const newtask = task - 1;
-    const allTask = parseInt(getInnerTextById("all-task"));
-    const newAllTask = allTask + 1;
-    alert("Board Updated Successfully");
-    document.getElementById("taskAssigned").innerText = newtask;
-    document.getElementById("all-task").innerText = newAllTask;
-    document.getElementById("fifth-btn").disabled = true;
-    document.getElementById("fifth-btn").style.backgroundColor = "#9ca3af";
-    const timeFormate = {
-      hour: "2-digit",
-      minute: "2-digit",
-      second: "2-digit",
-      hour12: true,
-    };
-    const time = new Intl.DateTimeFormat("en-US", timeFormate).format(
-      new Date()
-    );
-    const h1 = document.getElementById("fifth-head").innerText;
-    const p = document.createElement("p");
-    p.innerText = `You have complete the task ${h1} at ${time}`;
-    document.getElementById("history").appendChild(p);
-    p.classList.add(
-      "bg-gray-100",
-      "mt-4",
-      "text-xs",
-      "px-2",
-      "mx-3",
-      "rounded-lg",
-      "py-2",
-      "text-[#000000]"
-    );
+    task(event, "fifth-btn", "fifth-head");
   });
 // sixth-btn (sixth task)
 document
   .getElementById("sixth-btn")
   .addEventListener("click", function (event) {
-    event.preventDefault();
-    const task = parseInt(getInnerTextById("taskAssigned"));
-    const newtask = task - 1;
-    const allTask = parseInt(getInnerTextById("all-task"));
-    const newAllTask = allTask + 1;
-    alert("Board Updated Successfully");
-    document.getElementById("taskAssigned").innerText = newtask;
-    document.getElementById("all-task").innerText = newAllTask;
-    document.getElementById("sixth-btn").disabled = true;
-    document.getElementById("sixth-btn").style.backgroundColor = "#9ca3af";
-    const timeFormate = {
-      hour: "2-digit",
-      minute: "2-digit",
-      second: "2-digit",
-      hour12: true,
-    };
-    const time = new Intl.DateTimeFormat("en-US", timeFormate).format(
-      new Date()
-    );
-    const h1 = document.getElementById("sixth-head").innerText;
-    const p = document.createElement("p");
-    p.innerText = `You have complete the task ${h1} at ${time}`;
-    document.getElementById("history").appendChild(p);
-    p.classList.add(
-      "bg-gray-100",
-      "mt-4",
-      "text-xs",
-      "px-2",
-      "mx-3",
-      "rounded-lg",
-      "py-2",
-      "text-[#000000]"
-    );
+    task(event, "sixth-btn", "sixth-head");
   });
 // clear btn
 document
--- script/utilitis.js
+++ script/utilitis.js
@@ -3,3 +3,37 @@ function getInnerTextById(id) {
   const inner = document.getElementById(id).innerText;
   return inner;
 }
+// function for complete task
+function task(event, btnid, heading) {
+  event.preventDefault();
+  const task = parseInt(getInnerTextById("taskAssigned"));
+  const newtask = task - 1;
+  const allTask = parseInt(getInnerTextById("all-task"));
+  const newAllTask = allTask + 1;
+  alert("Board Updated Successfully");
+  document.getElementById("taskAssigned").innerText = newtask;
+  document.getElementById("all-task").innerText = newAllTask;
+  document.getElementById(btnid).disabled = true; // "shopEase-btn"
+  document.getElementById(btnid).style.backgroundColor = "#9ca3af";
+  const timeFormate = {
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: true,
+  };
+  const time = new Intl.DateTimeFormat("en-US", timeFormate).format(new Date());
+  const h1 = document.getElementById(heading).innerText;
+  const p = document.createElement("p");
+  p.innerText = `You have complete the task ${h1} at ${time}`;
+  document.getElementById("history").appendChild(p);
+  p.classList.add(
+    "bg-gray-100",
+    "mt-4",
+    "text-xs",
+    "px-2",
+    "mx-3",
+    "rounded-lg",
+    "py-2",
+    "text-[#000000]"
+  );
+}
